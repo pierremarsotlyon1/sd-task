8f852d41fa05ab297a528f88af154d5575e3a41a
pierremarsot <pierre.marsot@etu.univ-lyon1.fr>
2022-01-10 22:35:41 +0100

    max btn + refresh data + stake account + separate withdraw

--- pages/solanastaking.js
+++ pages/solanastaking.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { getPhantomProvider, getPhantomPublicKey, isPhantomConnected } from '../tools/phantom/phantom';
+import { getConnection, getPhantomProvider, getPhantomPublicKey, getStakeAccounts, isPhantomConnected } from '../tools/phantom/phantom';
 const web3 = require("@solana/web3.js");
 
 export default class Bonds extends React.Component {
@@ -12,6 +12,8 @@ export default class Bonds extends React.Component {
         balance: 0,
         error: null,
         stakeState: "",
+        balanceToStake: "",
+        stakeAccounts: [],
     };
 
     componentDidMount() {
@@ -21,6 +23,7 @@ export default class Bonds extends React.Component {
             window.solana.on('disconnect', this.handleConnect);
 
             this.setBalance();
+            this.setStakeAccounts();
             this.setState({ isConnected: isPhantomConnected() });
         }
     }
@@ -34,7 +37,7 @@ export default class Bonds extends React.Component {
         }
 
         this.setState({ airdropPending: true }, () => {
-            const connection = new web3.Connection(web3.clusterApiUrl('devnet'), 'confirmed');
+            const connection = getConnection();
             connection.requestAirdrop(
                 window.solana.publicKey,
                 web3.LAMPORTS_PER_SOL,
@@ -59,10 +62,29 @@ export default class Bonds extends React.Component {
         if (!window?.solana?.publicKey) {
             return;
         }
-        const connection = new web3.Connection(web3.clusterApiUrl('devnet'), 'confirmed');
+        const connection = getConnection();
         const balance = await connection.getBalance(window.solana.publicKey);
         this.setState({ balance: balance / 1000000000 });
     };
+
+    /**
+     * Get all info about stake accounts + rewards
+     */
+    setStakeAccounts = async () => {
+        if (!window?.solana?.publicKey) {
+            return;
+        }
+
+        const connection = getConnection();
+        const stakeAccounts = await getStakeAccounts(connection, window.solana.publicKey);
+
+        for (const stakeAccount of stakeAccounts) {
+            const stakeBalance = await connection.getBalance(stakeAccount.pubkey);
+            stakeAccount.stakeBalance = stakeAccount.account.lamports / 1000000000;
+            stakeAccount.rewards = stakeBalance / 1000000000 - stakeAccount.stakeBalance;
+        }
+        this.setState({ stakeAccounts });
+    }
 
     /**
      * Check if phantom is connected and fetch balance
@@ -70,7 +92,7 @@ export default class Bonds extends React.Component {
     handleConnect = () => {
         this.setState(
             { isConnected: isPhantomConnected() },
-            () => this.setBalance()
+            () => this.refreshStakingInfo()
         );
     };
 
@@ -82,11 +104,14 @@ export default class Bonds extends React.Component {
             return;
         }
 
-        const connection = new web3.Connection(web3.clusterApiUrl('devnet'), 'confirmed');
+        const connection = getConnection();
         const fromPublicKey = window.solana.publicKey;
 
         // Create stake account to manage the staking
         const stakeAccount = web3.Keypair.generate();
+
+        // Balance to stake
+        const balanceToStake = parseFloat(this.state.balanceToStake);
 
         let blockhashObj = await connection.getRecentBlockhash();
         const createAccountTransaction = new web3.Transaction({
@@ -96,7 +121,7 @@ export default class Bonds extends React.Component {
             web3.StakeProgram.createAccount({
                 fromPubkey: fromPublicKey,
                 authorized: new web3.Authorized(fromPublicKey, fromPublicKey),
-                lamports: 1000000000,
+                lamports: balanceToStake * 1000000000,
                 lockup: new web3.Lockup(0, 0, fromPublicKey),
                 stakePubkey: stakeAccount.publicKey
             })
@@ -110,6 +135,7 @@ export default class Bonds extends React.Component {
         await connection.confirmTransaction(signature);
 
         // Check that stake is available
+
         const stakeBalance = await connection.getBalance(stakeAccount.publicKey);
         console.log(`Stake balance: ${stakeBalance}`)
 
@@ -118,10 +144,7 @@ export default class Bonds extends React.Component {
         console.log(`Stake Stake: ${stakeState.state}`);
 
         // To delegate our stake, we get the current vote accounts and choose the first
-        const voteAccounts = await connection.getVoteAccounts();
-        const voteAccount = voteAccounts.current.concat(
-            voteAccounts.delinquent,
-        )[0];
+        const voteAccount = await this.getVoteAccount();
         const votePubkey = new web3.PublicKey(voteAccount.votePubkey);
 
         // We can then delegate our stake to the voteAccount
@@ -143,76 +166,95 @@ export default class Bonds extends React.Component {
 
         console.log("staking done");
 
-        const deactivateTransaction = new web3.Transaction({
-            feePayer: fromPublicKey,
-            recentBlockhash: blockhashObj.blockhash,
-        }).add(
-            web3.StakeProgram.deactivate({
-                stakePubkey: stakeAccount.publicKey,
-                authorizedPubkey: fromPublicKey,
-            })
-        );
-
-        signed = await window.solana.signTransaction(deactivateTransaction);
-        signature = await connection.sendRawTransaction(signed.serialize());
-        await connection.confirmTransaction(signature);
-
-        console.log("desactivate staking done");
-
-        const withdrawTransaction = new web3.Transaction({
-            feePayer: fromPublicKey,
-            recentBlockhash: blockhashObj.blockhash,
-        }).add(
-            web3.StakeProgram.withdraw({
-                stakePubkey: stakeAccount.publicKey,
-                authorizedPubkey: fromPublicKey,
-                toPubkey: fromPublicKey,
-                lamports: stakeBalance,
-            })
-        );
-        
-        signed = await window.solana.signTransaction(withdrawTransaction);
-        signature = await connection.sendRawTransaction(signed.serialize());
-        await connection.confirmTransaction(signature);
-        console.log("withdraw funds done");
+        // Refresh data and UI
+        this.refreshStakingInfo();
     };
+
+    /**
+     * Withdraw few SOL from delegator
+     */
+    withdraw = async (pubKey, stakeBalance) => {
+        console.log("stakeBalance : " + stakeBalance);
+        const connection = getConnection();
+        const fromPublicKey = window.solana.publicKey;
+        const blockhashObj = await connection.getRecentBlockhash();
+
+        // If stake account desactivated, we can withdraw funds directly
+        let stakeState = await connection.getStakeActivation(pubKey);
+        if (stakeState.state !== "inactive" && stakeState.state !== "deactivating") {
+            const deactivateTransaction = new web3.Transaction({
+                feePayer: fromPublicKey,
+                recentBlockhash: blockhashObj.blockhash,
+            }).add(
+                web3.StakeProgram.deactivate({
+                    stakePubkey: pubKey,
+                    authorizedPubkey: fromPublicKey,
+                })
+            );
+
+            let signed = await window.solana.signTransaction(deactivateTransaction);
+            let signature = await connection.sendRawTransaction(signed.serialize());
+            await connection.confirmTransaction(signature);
+        }
+
+        stakeState = await connection.getStakeActivation(pubKey);
+        if (stakeState.state === "inactive") {
+            // Withdraw all stake funds
+            const withdrawTransaction = new web3.Transaction({
+                feePayer: fromPublicKey,
+                recentBlockhash: blockhashObj.blockhash,
+            }).add(
+                web3.StakeProgram.withdraw({
+                    stakePubkey: pubKey,
+                    authorizedPubkey: fromPublicKey,
+                    toPubkey: fromPublicKey,
+                    lamports: stakeBalance,
+                })
+            );
+
+            let signed = await window.solana.signTransaction(withdrawTransaction);
+            let signature = await connection.sendRawTransaction(signed.serialize());
+            await connection.confirmTransaction(signature);
+        }
+        else {
+            this.showError("You could withdraw your funds when your stake account will be inactive");
+        }
+
+        this.refreshStakingInfo();
+    };
+
+    getVoteAccount = async () => {
+        const connection = getConnection();
+        const voteAccounts = await connection.getVoteAccounts();
+        return voteAccounts.current[0];
+    }
 
     /**
      * Get staking informations
      */
     refreshStakingInfo = async () => {
-        const connection = new web3.Connection(web3.clusterApiUrl('devnet'), 'confirmed');
-        const accountInfo = await connection.getAccountInfo(window.solana.publicKey);
-        console.log(accountInfo);
-
-        const accounts = await connection.getProgramAccounts(
-            accountInfo.owner, // new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
-            {
-              filters: [
-                {
-                  dataSize: 165, // number of bytes
-                },
-                {
-                  memcmp: {
-                    offset: 32, // number of bytes
-                    bytes: accountInfo.owner.toString(), // base58 encoded string
-                  },
-                },
-              ],
-            }
-          );
-          console.log(accounts);
-
-        console.log(accounts.filter(a => a.account.executable));
+        this.setStakeAccounts();
+        this.setBalance();
     };
+
+    /**
+     * Set the number of sol to delegate
+     */
+    balanceToStake = balanceToStake => this.setState({ balanceToStake });;
+
+    /**
+     * Set the max sol that can be delegate
+     * Minus small amount of sol for tx
+     */
+    setStakingMaxBalance = () => this.setState({ balanceToStake: (this.state.balance - 0.005).toString() });
 
     /**
      * Display an error in the UI
      */
     showError = error => {
-        this.setState({error}, () => {
+        this.setState({ error }, () => {
             setTimeout(() => {
-                this.setState({error: null});
+                this.setState({ error: null });
             }, 5000);
         });
     }
@@ -234,23 +276,51 @@ export default class Bonds extends React.Component {
         return (
             <div className="flex flex-row items-center justify-center mt-8">
                 <div className="solanadevnet">
-                    <h3 className='text-center'>Devnet {getPhantomPublicKey()}</h3>
-                    <p className='devnet-balance'>Balance : {this.state.balance}</p>
-                    <button
-                        className='px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm'
-                        onClick={() => this.requestAirdrop()}
-                    >Request airdrop</button>
-                    <button
-                        className='mt-2 px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm'
-                        onClick={() => this.stake()}
-                    >Stake my token</button>
+                    <div className='flex flex-col justify-center'>
+                        <h3 className='text-center'>Devnet {getPhantomPublicKey()}</h3>
+                        <p className='devnet-balance'>Balance : {this.state.balance}</p>
+                        <button
+                            className='mb-8 px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm'
+                            onClick={() => this.requestAirdrop()}
+                        >Request airdrop</button>
+                    </div>
 
-                    <h3 className='mt-5 text-center'>Staking informations</h3>
-                    <p>Stake state : {this.state.stakeState}</p>
-                    <button
-                        className='mt-2 px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm'
-                        onClick={() => this.refreshStakingInfo()}
-                    >Refresh</button>
+                    <div className='flex flex-col justify-center mt-6'>
+                        <div className='flex flex-row justify-between'>
+                            <input className='text-black' onChange={e => this.balanceToStake(e.target.value)} type="text" value={this.state.balanceToStake} />
+                            <button onClick={() => this.setStakingMaxBalance()}>Max</button>
+                        </div>
+                        <button
+                            className='px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm'
+                            onClick={() => this.stake()}
+                            disabled={!this.state.balanceToStake || this.state.balanceToStake === "0"}
+                        >Stake</button>
+                    </div>
+
+                    <div className='flex flex-col justify-center mt-6'>
+                        <h3 className='mt-5 text-center'>Stake accounts</h3>
+                        <ul>
+                            {
+                                this.state.stakeAccounts.map((stakeAccount, index) => {
+                                    return <li key={index} className='mb-8'>
+                                        <div className='flex flex-col'>
+                                            <p>SOL stake : {stakeAccount.stakeBalance}</p>
+                                            <p>Rewards : {stakeAccount.rewards} </p>
+                                            <button
+                                                className='mt-2 px-4 py-2 bg-orange-400 text-white text-base font-medium rounded-md shadow-sm'
+                                                onClick={() => this.withdraw(stakeAccount.pubkey, stakeAccount.account.lamports)}>
+                                                Withdraw all
+                                            </button>
+                                        </div>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                        <button
+                            className='mt-2 px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm'
+                            onClick={() => this.refreshStakingInfo()}
+                        >Refresh data</button>
+                    </div>
                     <p className='text-red text-center'>{this.state.error}</p>
                 </div>
             </div>
--- tools/phantom/phantom.js
+++ tools/phantom/phantom.js
@@ -1,3 +1,5 @@
+const web3 = require("@solana/web3.js");
+
 export const getPhantomProvider = () => {
     try {
         if ("solana" in window) {
@@ -38,3 +40,23 @@ export const getPhantomPublicKey = () => {
         return null;
     }
 };
+
+export function getConnection() {
+    return new web3.Connection(web3.clusterApiUrl('devnet'), 'confirmed');
+}
+
+export async function getStakeAccounts(connection, from) {
+    return await connection.getProgramAccounts(new web3.PublicKey("Stake11111111111111111111111111111111111111"),
+        {
+            encoding: "base64",
+            commitment: "confirmed",
+            filters: [
+                {
+                    memcmp: {
+                        bytes: from.toString(),
+                        offset: 12
+                    }
+                }
+            ]
+        });
+}
